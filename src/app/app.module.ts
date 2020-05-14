d6926cb2a69c455b2e078d712084d55f0fd3634a
Rahul Ranjan <ranjanrahul494@gmail.com>
2020-05-14 13:37:25 +0530

    added images, header and footers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
