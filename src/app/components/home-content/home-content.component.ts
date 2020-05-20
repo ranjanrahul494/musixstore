253ad8a837d109ee5f1fd0e52ad120a978caf47c
Rahul Ranjan <ranjanrahul494@gmail.com>
2020-05-21 00:49:49 +0530

    changes on footer , slidet and home content

--- src/app/components/home-content/home-content.component.ts
+++ src/app/components/home-content/home-content.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
   styleUrls: ['./home-content.component.scss']
 })
 export class HomeContentComponent implements OnInit {
+  
   musicNames = ['1', '2', '3', '4','5', '6'];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   popUp(){
